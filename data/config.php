97962dcf728e2319398f780489fd415818109dbd
Ryan Nagle <rmnagle@gmail.com>
2014-08-13 21:41:28 -0500

    remove description field from job types since it's not used anymore

--- data/config.php
+++ data/config.php
@@ -4,52 +4,42 @@ $wpjobboard_categories = array(
 	array(
 		'title' => 'Advocacy and Community Organizing',
 		'slug' => sanitize_title('Advocacy and Community Organizing'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Arts, Media, and Culture',
 		'slug' => sanitize_title('Arts, Media, and Culture'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Economic and Business',
 		'slug' => sanitize_title('Economic and Business'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Education and Academic',
 		'slug' => sanitize_title('Education and Academic'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Government',
 		'slug' => sanitize_title('Government'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Health and Environment',
 		'slug' => sanitize_title('Health and Environment'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Housing, Urban Planning, Real Estate',
 		'slug' => sanitize_title('Housing, Urban Planning, Real Estate'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Interns and Student Internships',
 		'slug' => sanitize_title('Interns and Student Internships'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Justice and Legal',
 		'slug' => sanitize_title('Justice and Legal'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Social Service and Agency',
 		'slug' => sanitize_title('Social Service and Agency'),
-		'description' => ''
 	)
 );
 
@@ -57,111 +47,89 @@ $wpjobboard_job_types = array(
 	array(
 		'title' => 'Urban Planning',
 		'slug' => sanitize_title('Urban Planning'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Transportation',
 		'slug' => sanitize_title('Transportation'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Technical/Information Technology',
 		'slug' => sanitize_title('Technical/Information Technology'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Sustainability and Development',
 		'slug' => sanitize_title('Sustainability and Development'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Research/Public Policy',
 		'slug' => sanitize_title('Research/Public Policy'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Real Estate',
 		'slug' => sanitize_title('Real Estate'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Program Management',
 		'slug' => sanitize_title('Program Management'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Other',
 		'slug' => sanitize_title('Other'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Operations',
 		'slug' => sanitize_title('Operations'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Media/Communications',
 		'slug' => sanitize_title('Media/Communications'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Marketing',
 		'slug' => sanitize_title('Marketing'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Human Resources',
 		'slug' => sanitize_title('Human Resources'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Housing',
 		'slug' => sanitize_title('Housing'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Government',
 		'slug' => sanitize_title('Government'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Finance',
 		'slug' => sanitize_title('Finance'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Executive',
 		'slug' => sanitize_title('Executive'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Education and Training',
 		'slug' => sanitize_title('Education and Training'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Development/Fundraising',
 		'slug' => sanitize_title('Development/Fundraising'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Arts',
 		'slug' => sanitize_title('Arts'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Advising and Counseling',
 		'slug' => sanitize_title('Advising and Counseling'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Administrative Support',
 		'slug' => sanitize_title('Administrative Support'),
-		'description' => ''
 	),
 	array(
 		'title' => 'Activism/Advocacy',
 		'slug' => sanitize_title('Activism/Advocacy'),
-		'description' => ''
 	)
 );
