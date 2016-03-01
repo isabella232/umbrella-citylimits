bd0e391bfa7b943312b128f518afada37ae609c9
Ben Keith <ben@benlk.com>
2016-03-01 18:10:29 -0500

    Remove requirement that a job board poster be logged into the site and have permission to post in order to post a job

--- wpjobboard/job-board/add.php
+++ wpjobboard/job-board/add.php
@@ -18,74 +18,6 @@
 ?>
 
 <div class="wpjb wpjb-page-add">
-  <?php if (is_user_logged_in()) { ?>
-    <?php if($can_post): ?>
-
-    <?php wpjb_add_job_steps(); ?>
-    <?php wpjb_flash() ?>
-    <form class="wpjb-form" action="<?php echo wpjb_link_to("step_preview") ?>" method="post" enctype="multipart/form-data">
-
-        <?php echo $form->renderHidden() ?>
-        <?php foreach($form->getReordered() as $group): ?>
-
-        <?php /* @var $group stdClass */ ?>
-        <fieldset class="wpjb-fieldset-<?php esc_attr_e($group->getName()) ?>">
-            <legend><?php esc_html_e($group->title) ?></legend>
-            <?php foreach($group->getReordered() as $name => $field): ?>
-            <?php /* @var $field Daq_Form_Element */ ?>
-            <div class="<?php wpjb_form_input_features($field) ?>">
-
-                <label class="wpjb-label">
-                    <?php esc_html_e($field->getLabel()) ?>
-                    <?php if($field->isRequired()): ?><span class="wpjb-required">*</span><?php endif; ?>
-                </label>
-
-                <div class="wpjb-field">
-                    <?php wpjb_form_render_input($form, $field) ?>
-                    <?php wpjb_form_input_hint($field) ?>
-                    <?php wpjb_form_input_errors($field) ?>
-                </div>
-
-            </div>
-            <?php endforeach; ?>
-        </fieldset>
-        <?php endforeach; ?>
-
-        <fieldset>
-            <div>
-                <legend></legend>
-
-                <?php if($show_pricing): ?>
-
-                <div id="wpjb_pricing" class="wpjb-grid wpjb-grid-closed-top wpjb-grid-compact">
-                    <div class="wpjb-grid-row">
-                        <div class="wpjb-col-30"><?php _e("Listing cost", "wpjobboard") ?></div>
-                        <div class="wpjb-col-65"><span id="wpjb-listing-cost">-</span></div>
-                    </div>
-                    <div class="wpjb-grid-row">
-                        <div class="wpjb-col-30"><?php _e("Discount", "wpjobboard") ?></div>
-                        <div class="wpjb-col-65"><span id="wpjb-listing-discount">-</span></div>
-                    </div>
-                    <div class="wpjb-grid-row">
-                        <div class="wpjb-col-30"><strong><?php _e("Total", "wpjobboard") ?>:</strong></div>
-                        <div class="wpjb-col-65"><strong><span id="wpjb-listing-total">-</span></strong></div>
-                    </div>
-
-                </div>
-
-                <?php endif; ?>
-
-                <div style="margin:1em 0 1em 0">
-                    <input type="submit" class="wpjb-submit" name="wpjb_preview" id="wpjb_submit" value="<?php _e("Preview", "wpjobboard") ?>" />
-                    <?php _e("or", "wpjobboard") ?>
-                    <a href="<?php esc_attr_e(wpjb_link_to("step_reset")) ?>"><?php _e("Reset form", "wpjobboard") ?></a>
-                </div>
-            </div>
-        </fieldset>
-
-    </form>
-    <?php endif; ?>
-  <?php } else { ?>
 	<div class="wpjb-login-message">
 		<p>Reach the most educated and engaged job candidates for your organization:</p>
 
@@ -94,16 +26,72 @@
 			<p><strong>Premium Posting:</strong> 30 Day Featured Posting on the City Limits Job Board, plus featured listing in the Work Site section and the Weekly Reads Newsletter</p>
 		</div>
 
-		<p>Here's how to post:</p>
-
-		<ol>
-			<li>1) If you have an account, log in to post your job. If you don't have an account, you will quickly need to <a href="<?php echo home_url('/register'); ?>">register here</a>.</li>
-			<li>2) After you have registered, you will simply need to click on the Post a Job Link again to log-in</li>
-			<li>3) Once logged-in, simply input your job-posting into the form provided.</li>
-			<li>4) To activate your posting, simply check out via Paypal and your job will be posted within 2 hours.</li>
-		</ol>
+		<p>To activate your posting, simply check out via Paypal and your job will be posted within 2 hours.</p>
 
 		<p>If you have questions, please contact <a href="mailto:advertise@citylimits.org">advertise@citylimits.org</a>. Thank you!</p>
 	</div>
-  <?php } ?>
+
+	<?php wpjb_add_job_steps(); ?>
+	<?php wpjb_flash() ?>
+	<form class="wpjb-form" action="<?php echo wpjb_link_to("step_preview") ?>" method="post" enctype="multipart/form-data">
+
+		<?php echo $form->renderHidden() ?>
+		<?php foreach($form->getReordered() as $group): ?>
+
+		<?php /* @var $group stdClass */ ?>
+		<fieldset class="wpjb-fieldset-<?php esc_attr_e($group->getName()) ?>">
+			<legend><?php esc_html_e($group->title) ?></legend>
+			<?php foreach($group->getReordered() as $name => $field): ?>
+			<?php /* @var $field Daq_Form_Element */ ?>
+			<div class="<?php wpjb_form_input_features($field) ?>">
+
+				<label class="wpjb-label">
+					<?php esc_html_e($field->getLabel()) ?>
+					<?php if($field->isRequired()): ?><span class="wpjb-required">*</span><?php endif; ?>
+				</label>
+
+				<div class="wpjb-field">
+					<?php wpjb_form_render_input($form, $field) ?>
+					<?php wpjb_form_input_hint($field) ?>
+					<?php wpjb_form_input_errors($field) ?>
+				</div>
+
+			</div>
+			<?php endforeach; ?>
+		</fieldset>
+		<?php endforeach; ?>
+
+		<fieldset>
+			<div>
+				<legend></legend>
+
+				<?php if($show_pricing): ?>
+
+				<div id="wpjb_pricing" class="wpjb-grid wpjb-grid-closed-top wpjb-grid-compact">
+					<div class="wpjb-grid-row">
+						<div class="wpjb-col-30"><?php _e("Listing cost", "wpjobboard") ?></div>
+						<div class="wpjb-col-65"><span id="wpjb-listing-cost">-</span></div>
+					</div>
+					<div class="wpjb-grid-row">
+						<div class="wpjb-col-30"><?php _e("Discount", "wpjobboard") ?></div>
+						<div class="wpjb-col-65"><span id="wpjb-listing-discount">-</span></div>
+					</div>
+					<div class="wpjb-grid-row">
+						<div class="wpjb-col-30"><strong><?php _e("Total", "wpjobboard") ?>:</strong></div>
+						<div class="wpjb-col-65"><strong><span id="wpjb-listing-total">-</span></strong></div>
+					</div>
+
+				</div>
+
+				<?php endif; ?>
+
+				<div style="margin:1em 0 1em 0">
+					<input type="submit" class="wpjb-submit" name="wpjb_preview" id="wpjb_submit" value="<?php _e("Preview", "wpjobboard") ?>" />
+					<?php _e("or", "wpjobboard") ?>
+					<a href="<?php esc_attr_e(wpjb_link_to("step_reset")) ?>"><?php _e("Reset form", "wpjobboard") ?></a>
+				</div>
+			</div>
+		</fieldset>
+
+	</form>
 </div>
