3714914aac31731d45fbf71f4817bc83e1f9b4c0
rclations <contact@rclations.com>
2016-09-29 08:09:31 -0400

    template updates & rezone logo

--- themes/citylimits/page-neighborhoods.php
+++ themes/citylimits/page-neighborhoods.php
@@ -32,40 +32,40 @@ get_header();
 	</div>
 </div>
 
-<?php while ( have_posts() ) : the_post(); ?>
-	<section class="rezone-overview">
-		<div class="row-fluid">
-			<div class="span12">
-					<?php the_content(); ?>
-			</div>
+<section class="rezone-overview">
+	<div class="row-fluid">
+		<div class="span12">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; ?>
 		</div>
+	</div>
 
-		<?php
-		$args = array(
-		    'posts_per_page' => 3,
-		    'order'          => 'DESC',
-		    'post_parent'    => $post->ID,
-		    'post_type'      => 'page'
-		    );
+	<?php
+	$args = array(
+	    'posts_per_page' => 3,
+	    'order'          => 'DESC',
+	    'post_parent'    => $post->ID,
+	    'post_type'      => 'page'
+	    );
 
-		$get_children_array = get_children( $args,ARRAY_A );  //returns Array ( [$image_ID].
-		?>
+	$get_children_array = get_children( $args,ARRAY_A );  //returns Array ( [$image_ID].
+	?>
 
-		<?php if ( count( $get_children_array ) > 0 ) : ?>
-			<div class="row-fluid">
-				<?php foreach ( $get_children_array as $child ) : ?>
-					<?php setup_postdata( get_post( $child['ID'] ) ); ?>
-					<div class="span4">
-						<h3><?php echo '<a href="' . get_permalink( $child['ID'] ) . '" title="' . get_the_title( $child['ID'] ) . '">' .  get_the_title( $child['ID'] ) . '</a>'; ?></h3>
-						<p><?php echo get_the_excerpt( $child['ID'] ); ?></p>
-						<?php echo '<a href="' . get_permalink( $child['ID'] ) . '" title="' . get_the_title( $child['ID'] ) . '">Read More ></a>'; ?>
-					</div>
-				<?php endforeach; ?>
-			</div>
-		<?php endif; ?>
-		<?php rewind_posts(); ?>
-	</section>
-<?php endwhile; ?>
+	<?php if ( count( $get_children_array ) > 0 ) : ?>
+		<div class="row-fluid">
+			<?php foreach ( $get_children_array as $child ) : ?>
+				<?php setup_postdata( get_post( $child['ID'] ) ); ?>
+				<div class="span4">
+					<h3><?php echo '<a href="' . get_permalink( $child['ID'] ) . '" title="' . get_the_title( $child['ID'] ) . '">' .  get_the_title( $child['ID'] ) . '</a>'; ?></h3>
+					<p><?php echo get_the_excerpt( $child['ID'] ); ?></p>
+					<?php echo '<a href="' . get_permalink( $child['ID'] ) . '" title="' . get_the_title( $child['ID'] ) . '">Read More ></a>'; ?>
+				</div>
+			<?php endforeach; ?>
+		</div>
+	<?php endif; ?>
+	<?php rewind_posts(); ?>
+</section>
 
 <section>
 	<div class="row-fluid">
