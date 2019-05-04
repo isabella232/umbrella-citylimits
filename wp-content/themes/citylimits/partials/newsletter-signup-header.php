a1c5030e4ac623b1aacad44863da98dfdb08a44d
jonny <megabulk@grundig9000.com>
2019-05-04 19:00:31 -0400

    added tagline to header

--- wp-content/themes/citylimits/partials/newsletter-signup-header.php
+++ wp-content/themes/citylimits/partials/newsletter-signup-header.php
@@ -1,7 +1,11 @@
 <div class="newsletter-signup header">
+	<div class="cl-tagline-holder">
+		<div class="cl-tagline">
+		</div>
+	</div>
 	 <? if ($_COOKIE['signedUpNewsletter'] == true) { ?>
-	<a href="<?= site_url() ?>/get-the-city-limits-weekly-brief/" class="header-cl-ad"><img src="<?= get_stylesheet_directory_uri() ?>/img/newsletter_signup_header.gif"></a>
-	<? } else { ?>
 	<a href="<?= site_url() ?>/donate?campaign=7011U000000M5rC" class="header-cl-ad"><img src="<?= get_stylesheet_directory_uri() ?>/img/donate_header.gif"></a>
+	<? } else { ?>
+	<a href="<?= site_url() ?>/get-the-city-limits-weekly-brief/" class="header-cl-ad"><img src="<?= get_stylesheet_directory_uri() ?>/img/newsletter_signup_header.gif"></a>
 	<? } ?>
 </div><!--.newsletter-signup-->
