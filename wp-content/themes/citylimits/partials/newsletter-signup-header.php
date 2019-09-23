a5135a1f0f6288c129dc97f75e25023314f96523
Ben Keith <ben@benlk.com>
2019-09-23 17:14:28 -0400

    Fix php short tag error in newsletter header

--- wp-content/themes/citylimits/partials/newsletter-signup-header.php
+++ wp-content/themes/citylimits/partials/newsletter-signup-header.php
@@ -3,9 +3,9 @@
 		<div class="cl-tagline">
 		</div>
 	</div>
-	 <?php if ( $_COOKIE['signed_up_for_newsletter'] == true || $post->post_name == 'newsletter-subscriptions' ) { ?>
-	<a href="<?= site_url() ?>/donate?campaign=7011U000000M5rC" class="header-cl-ad"><img src="<?= get_stylesheet_directory_uri() ?>/img/header_donate.svg"></a>
-	<? } else { ?>
-	<a href="<?= site_url() ?>/newsletter-subscriptions/" class="header-cl-ad header_newsletter_signup"><img src="<?= get_stylesheet_directory_uri() ?>/img/header_newsletter_signup.svg"></a>
-	<? } ?>
+	<?php if ( $_COOKIE['signed_up_for_newsletter'] == true || $post->post_name == 'newsletter-subscriptions' ) { ?>
+		<a href="<?php echo site_url(); ?>/donate?campaign=7011U000000M5rC" class="header-cl-ad"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/header_donate.svg"></a>
+	<?php } else { ?>
+		<a href="<?php echo site_url(); ?>/newsletter-subscriptions/" class="header-cl-ad header_newsletter_signup"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/header_newsletter_signup.svg"></a>
+	<?php } ?>
 </div><!--.newsletter-signup-->
