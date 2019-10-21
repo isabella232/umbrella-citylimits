6275a2b16fa000c2b6b62bcc5c6bd0c22eec27df
Ben Keith <ben@benlk.com>
2019-10-20 23:35:47 -0400

    add lines between list items on mobile, and make the first image be bigger

--- wp-content/themes/citylimits/partials/widget-content.php
+++ wp-content/themes/citylimits/partials/widget-content.php
@@ -17,20 +17,26 @@ if ($thumb == 'small') {
 	$img_attr = array( 'class' => $img_location . '-align' );
 	$img_attr['class'] .= " attachment-small";
 	?>
-		<a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail( get_the_ID(), '60x60', $img_attr); ?></a>
+		<a href="<?php echo get_permalink(); ?>" class="img">
+			<?php echo get_the_post_thumbnail( get_the_ID(), '60x60', $img_attr); ?>
+		</a>
 	<?php
 } elseif ($thumb == 'medium') {
 	$img_location = ! empty( $instance['image_align'] ) ? $instance['image_align'] : 'left';
 	$img_attr = array('class' => $img_location . '-align');
 	$img_attr['class'] .= " attachment-thumbnail";
 	?>
-		<a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail( get_the_ID(), 'post-thumbnail', $img_attr); ?></a>
+		<a href="<?php echo get_permalink(); ?>" class="img">
+			<?php echo get_the_post_thumbnail( get_the_ID(), 'post-thumbnail', $img_attr); ?>
+		</a>
 	<?php
 } elseif ($thumb == 'large') {
 	$img_attr = array();
 	$img_attr['class'] = " attachment-large";
 	?>
-		<a href="<?php echo get_permalink(); ?>"><?php echo get_the_post_thumbnail( get_the_ID(), 'large', $img_attr); ?></a>
+		<a href="<?php echo get_permalink(); ?>"class="img">
+			<?php echo get_the_post_thumbnail( get_the_ID(), 'large', $img_attr); ?>
+		</a>
 	<?php
 }
 
