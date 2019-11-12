299c67e0f2ff8cfd735283b9feb8522b9e3b1efd
Ben Keith <ben@benlk.com>
2019-11-12 16:45:20 -0500

    Remove automatically-positioned newsletter box from many positions; leave it at the top of the default series and archive templates.

--- wp-content/themes/citylimits/functions.php
+++ wp-content/themes/citylimits/functions.php
@@ -347,19 +347,11 @@ add_filter( 'max_srcset_image_width', 'set_max_srcset_image_width' );
 /**
  * newsletter subscribe forms
  */
-
 function citylimits_newsletter_form_interstitial() {
 	get_template_part( 'partials/newsletter-signup', 'maincolumn' );
 }
-add_action( 'largo_before_sticky_posts', 'citylimits_newsletter_form_interstitial', 11 );
-add_action( 'largo_category_after_primary_featured_post', 'citylimits_newsletter_form_interstitial', 11 );
 add_action( 'largo_series_before_stories', 'citylimits_newsletter_form_interstitial', 11 );
 add_action( 'largo_archive_before_stories', 'citylimits_newsletter_form_interstitial', 11 );
-
-function citylimits_newsletter_form_footer() {
-	get_template_part( 'partials/newsletter-signup', 'footer' );
-}
-add_action( 'largo_before_footer', 'citylimits_newsletter_form_footer', 11 );
 
 function citylimits_newsletter_form_popover() {
 	get_template_part( 'partials/newsletter-signup', 'popover' );
