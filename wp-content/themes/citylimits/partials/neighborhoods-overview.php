78b0ee935c15e7551704a6310009079f391dcc5c
Josh Darby <josh@inn.org>
2020-02-21 10:12:55 -0500

    Add new series header to old series template

--- wp-content/themes/citylimits/partials/neighborhoods-overview.php
+++ wp-content/themes/citylimits/partials/neighborhoods-overview.php
@@ -8,4 +8,21 @@
 		</div>
 	</div>
 </section>
+<div class="series-header-container">
+	<div class="series-banner">
+		<?php the_post_thumbnail( 'large' ); ?>
+	</div>
+	<section id="series-header" class="">
+		<span class="special-project"><?php esc_html_e( 'Special Project', 'citylimits' ); ?></span>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php
+			if ( $opt['show_series_byline'] ) {
+				echo '<h5 class="byline">' . largo_byline( false, false, get_the_ID() ) . '</h5>';
+			}
+		?>
+			<div class="description">
+				<?php echo apply_filters( 'the_content', $post->post_excerpt ); ?>
+			</div>
+	</section>
+</div>
 
